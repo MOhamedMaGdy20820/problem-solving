4102e9ca94f1a70d354efc0242388a200bd5e816
Mohamed Magdy <120112354+MOhamedMaGdy20820@users.noreply.github.com>
2023-02-25 09:44:04 +0200

    e5tsarat

---
+++ e5tsarat.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <string>
+#include <map>
+#define FIO cin.tie(0), cin.sync_with_stdio(0)
+using namespace std;
+typedef long long ll;
+#define all(x) x.begin(), x.end()
+#define fast cin.tie(0); cin.sync_with_stdio(0); cout.tie(0);
+
+#define loop(n)          for(int i = 0; i < (n); i++)
+#define lp(x, s, e)      for(int x = (s); x < (e); x++)
+#define lpe(x, s, e)     for(int x = (s); x <= (e); x++)
+#define loop_r(n)          for(int i = (n) - 1; i >= 0; i--)
+#define lp_r(x, s, e)      for(int x = (e) - 1; x >= (s); x--)
+#define lpe_r(x, s, e)     for(int x = (e); x >= (s); x--)
+#define MP           make_pair
+#define PB            push_back
+#define EmB         emplace_back
+#define Em            emplace
+#define len(s)        (int)s.length()
+#define sz(v)          (int)v.size()
+#define all(a)          a.begin(),a.end()
+#define all_r(a)      a.rbegin(),a.rend()
+#define clr(x, val)  memset((x), (val), sizeof(x))
+#define maxEle      *max_element
+#define minEle       *min_element
+
+#define tests int t; cin >> t; int i = 0; while(i++ < t)
+#define SetPre(n, x)  cout << fixed << setprecision(n) << x
+
+#define endl '\n'
+#define kill return 0
+
+int main() {
+    return 0;
+}
+
